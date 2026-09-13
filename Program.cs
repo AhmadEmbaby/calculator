d4787a0f21d928ecf70f29bda9fd7969aa5f7702
Ahmed Embaby <ahmedembaby099@gmail.com>
2026-09-13 18:21:15 +0300

    fix: handle input validation

--- Program.cs
+++ Program.cs
@@ -20,20 +20,77 @@
 }
 
 
-Console.Write("Enter first number: ");
-
-double num1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Enter an operation type [ + , - , *  ,  / ] : ");
-
-char operationType = Convert.ToChar(Console.ReadLine());
-
-Console.Write("Enter second number: ");
-double num2 = Convert.ToDouble(Console.ReadLine());
+static bool IsValidNumber(string input, out double number)
+{
+    return double.TryParse(input, out number);
+}
 
 
+static bool IsValidOperation(string input, out char operationType)
+{
+    if (char.TryParse(input, out operationType) &&
+        "+-*/".Contains(operationType))
+    {
+        return true;
+    }
 
-double result = Calculate(num1, num2, operationType);
-Console.WriteLine( $"\n\n_______________Result________________\n" );
-Console.WriteLine( $" {num1} {operationType} {num2} = {result}");
-Console.WriteLine($"\n______________________________________\n");
+    return false;
+}
+
+
+bool calcAgain = true;
+
+while (calcAgain)
+{
+    Console.Write("Enter first number: ");
+
+    if (!IsValidNumber(Console.ReadLine(), out double num1))
+    {
+        Console.WriteLine("Invalid number.");
+        continue;
+    }
+
+    Console.Write("Enter an operation type [ + , - , *  ,  / ] : ");
+
+    if (!IsValidOperation(Console.ReadLine(), out char operationType))
+    {
+        Console.WriteLine("Invalid operation.");
+        continue;
+    }
+
+    Console.Write("Enter second number: ");
+
+    if (!IsValidNumber(Console.ReadLine(), out double num2))
+    {
+        Console.WriteLine("Invalid number.");
+        continue;
+    }
+
+
+    if (operationType == '/' && num2 == 0)
+    {
+        Console.WriteLine("Cannot divide by zero.");
+        continue;
+    }
+
+
+
+    double result = Calculate(num1, num2, operationType);
+    Console.WriteLine($"\n\n_______________Result________________\n");
+    Console.WriteLine($" {num1} {operationType} {num2} = {result}");
+    Console.WriteLine($"\n______________________________________\n");
+
+
+    Console.Write("Do you want to perform another calculation? [y/n]: ");
+
+    char userChoice = Convert.ToChar(Console.ReadLine());
+
+    if (userChoice ==  'y')
+    {
+        calcAgain = true;
+    }
+    else
+    {
+        calcAgain = false;
+    }
+}
